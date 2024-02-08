6beda8dadd8ef8819a07d385ffb99e608dec3ac9
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-08 15:14:08 +0900

    Title: 데이터 교환, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240208/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp1 = 0;
+        int temp2 = 0;
+
+        temp1 = b;
+        b = a;
+        temp2 = c;
+        c = temp1;
+        a = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
