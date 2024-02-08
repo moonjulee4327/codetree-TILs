c6de764a52f2ee5a24fbfdce457ac45612c9607f
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-08 14:35:53 +0900

    Title: 정수 선언하고 곱 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240208/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 26;
+        int y = 5;
+        
+        System.out.println(x + " * " + y + " = " + (x*y));
+    }
+}
