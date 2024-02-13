f2e247ad12b37318e09128c9a877de01a5ec6ba8
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-13 16:59:42 +0900

    Title: 살 수 있는 물건 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240213/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int bookPrice = 3000;
+        int maskPrice = 1000;
+        int penPrice = 500;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n >= bookPrice){
+            System.out.println("book");
+        } else if (n >= maskPrice) {
+            System.out.println("mask");
+        } else if (n >= penPrice) {
+            System.out.println("pen");
+        }else {
+            System.out.println("no");
+        }
+    }
+}
