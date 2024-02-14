72102fa21cf665a505c1250814ef3f3804c4829d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-14 20:01:10 +0900

    Title: 중앙값 구하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240214/중앙값 구하기/find-the-median.java
+++ 240214/중앙값 구하기/find-the-median.java
@@ -11,23 +11,32 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-
-        if (a > b){
-            if (b > c){
+        
+        if(a > b) {
+            if(c > a) {
+                // a > b, c > a 일때 (c > a > b)
+                System.out.println(a);
+            }
+            // a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+            else if(b > c) {
                 System.out.println(b);
-            }else if (c > a) {
-                System.out.println(a);
-            }else {
+            }
+            else {
                 System.out.println(c);
             }
-        }else if (b > c){
-            if (a > c){
+        }
+        else {
+            if(c > b) {
+                // b > a, c > b 일때 (c > b > a)
+                System.out.println(b);
+            }
+            // b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+            else if(a > c) {
                 System.out.println(a);
-            }else {
+            }
+            else {
                 System.out.println(c);
             }
-        }else {
-            System.out.println(b);
         }
     }
 }
