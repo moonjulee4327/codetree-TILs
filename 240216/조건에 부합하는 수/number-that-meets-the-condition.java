d10158f048f7ce9225ab124e75f6a2ab40067692
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-16 01:57:22 +0900

    Title: 조건에 부합하는 수, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240216/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 240216/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -9,9 +9,10 @@ public class Main {
 
         String str = "";
         for (int i = 1; i <= a; i++){
-            if (!((i % 2 == 0 && i % 4 != 0) || ((i / 8) % 2 == 0) || i % 7 < 4)){
-                str += i + " ";
+            if ((i % 2 == 0 && i % 4 != 0) || ((i / 8) % 2 == 0) || i % 7 < 4){
+                continue;
             }
+            str += i + " ";
         }
         System.out.println(str);
         br.close();
