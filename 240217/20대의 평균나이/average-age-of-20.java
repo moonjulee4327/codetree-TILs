7ef06d670bfc6e4aca3b7bfac7b190095969641d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-02-17 22:58:58 +0900

    Title: 20대의 평균나이, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 240217/20대의 평균나이/average-age-of-20.java
+++ 240217/20대의 평균나이/average-age-of-20.java
@@ -10,7 +10,7 @@ public class Main {
         int cnt = 0;
         while (true){
             int age = Integer.parseInt(br.readLine());
-            if (age >= 30){
+            if (age >= 30 || age < 20){
                 break;
             }
             sum += age;
