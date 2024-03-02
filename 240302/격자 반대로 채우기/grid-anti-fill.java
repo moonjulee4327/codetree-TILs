6db0fc74187fc50b43990c457fbe1d1f489ab0d1
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-03-02 23:43:15 +0900

    Title: 격자 반대로 채우기, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240302/격자 반대로 채우기/grid-anti-fill.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[n][n];
+        int count = 1;
+        
+        for (int i = n - 1; i >= 0; i--){
+            if (i % 2 == 0){
+                for (int j = 0; j < n; j++){
+                    arr[j][i] = count++;
+                }
+            }else {
+                for (int j = n - 1; j >= 0; j--){
+                    arr[j][i] = count++;
+                }
+            }
+        }
+
+        for (int[] numArr : arr){
+            for (int numArr1 : numArr){
+                System.out.print(numArr1 + " ");
+            }
+            System.out.println();
+        }
+
+        br.close();
+    }
+}
