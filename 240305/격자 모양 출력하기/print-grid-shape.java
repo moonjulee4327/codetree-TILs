77fba564b00b90fa83c7b4d808c04159ff787064
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-03-05 17:23:14 +0900

    Title: 격자 모양 출력하기, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 240305/격자 모양 출력하기/print-grid-shape.java
+++ 240305/격자 모양 출력하기/print-grid-shape.java
@@ -9,14 +9,13 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int n = Integer.parseInt(st.nextToken());
-        int n2 = Integer.parseInt(st.nextToken());
-        int[][] inputArr = new int[n2][2];
+        int m = Integer.parseInt(st.nextToken());
+        int[][] inputArr = new int[m][2];
         int[][] outputArr = new int[n][n];
-        int i;
-        int j;
-        for (i = 0; i < n2; i++){
+
+        for (int i = 0; i < m; i++){
             StringTokenizer inputSt = new StringTokenizer(br.readLine(), " ");
-            for (j = 0; j < 2; j++){
+            for (int j = 0; j < 2; j++){
                 inputArr[i][j] = Integer.parseInt(inputSt.nextToken());
             }
             outputArr[inputArr[i][0] - 1][inputArr[i][1] - 1] = inputArr[i][0] * inputArr[i][1];
