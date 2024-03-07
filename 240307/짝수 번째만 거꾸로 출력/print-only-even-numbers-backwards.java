a4df5842e2c56535b02b9e14ccf957c766e7b8c8
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-03-07 20:43:14 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 215ms, Memory: 8MB, Status: Passed - CodeTree

--- 240307/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240307/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -9,7 +9,7 @@ public class Main {
 
         char[] staCharArr = str.toCharArray();
         String result = "";
-        for (int i = staCharArr.length; i > 0; i--){
+        for (int i = staCharArr.length - 1; i > 0; i--){
             if(i % 2 == 1){
                 result += staCharArr[i];
             }
