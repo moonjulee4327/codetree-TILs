66e61c39a89abaa4fa3674304a784e652982b14b
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-03-12 23:03:34 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240312/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        printNLines();
+    }
+
+    public static void printNLines(){
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
