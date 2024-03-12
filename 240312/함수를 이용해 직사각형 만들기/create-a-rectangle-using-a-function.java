5672e6716fda653dcea39cedbadeb6986df3611a
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-03-12 23:17:08 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240312/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        printRect(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        br.close();
+    }
+
+    public static void printRect(int n, int m){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
